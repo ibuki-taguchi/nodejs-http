80e3a4b71799bc20e3574ae39dd1774a014d5823
ibuki <ibuki-taguchi@cybozu.co.jp>
2021-09-19 19:11:36 +0900

    #update index.js サーバーのログ・エラーログを出力対応

--- index.js
+++ index.js
@@ -7,19 +7,34 @@ fs.readFile("./www/index.html", "UTF-8", (err, file) => {
   if (err) {
     console.log("ファイルの読み込みでエラーが発生しました\n" + err);
   } else {
-    const server = http.createServer((req, res) => {
-      res.writeHead(200, {
-        "Content-Type": "text/html; charset=utf-8",
+    const server = http
+      .createServer((req, res) => {
+        console.info(
+          "[" +
+            new Date() +
+            "] Requested by " +
+            req.socket.remoteAddress +
+            req.headers["user-agent"]
+        );
+
+        res.writeHead(200, {
+          "Content-Type": "text/html; charset=utf-8",
+        });
+        res.write(file);
+
+        res.end();
+      })
+      .on("error", (e) => {
+        console.error("[" + new Date() + "] Server Error", e);
+      })
+      .on("clientError", (e) => {
+        console.error("[" + new Date() + "] Client Error", e);
       });
-      res.write(file);
-      console.log("access from  " + req.headers["user-agent"]);
-      res.end();
-    });
 
     // listen
     const port = 8000;
     server.listen(port, () => {
-      console.log("listening on..." + port);
+      console.info("[" + new Date() + "] listening on..." + port);
     });
   }
 });
