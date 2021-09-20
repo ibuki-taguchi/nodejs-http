1e36b3d3c99144765129208c5eea67a378c6d3ad
ibuki <ibuki-taguchi@cybozu.co.jp>
2021-09-21 08:36:57 +0900

    #update pugを使用できるように変更、/,/enquetes"にアクセスした際の挙動を実装

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM --platform=linux/x86_64 node:14.15.4
-
-RUN apt-get update
-RUN apt-get install -y locales tmux vim
-RUN locale-gen ja_JP.UTF-8
-RUN localedef -f UTF-8 -i ja_JP ja_JP
-ENV LANG ja_JP.UTF-8
-ENV TZ Asia/Tokyo
-WORKDIR /app
+ FROM --platform=linux/x86_64 node:14.15.4
+ 
+ RUN apt-get update
+ RUN apt-get install -y locales vim tmux
+ RUN locale-gen ja_JP.UTF-8
+ RUN localedef -f UTF-8 -i ja_JP ja_JP
+ ENV LANG ja_JP.UTF-8
+ ENV TZ Asia/Tokyo
+ RUN yarn add pug@2.0.4
+ RUN yarn global add pug-cli
+ WORKDIR /app
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 const http = require("http");
+const pug = require("pug");
 const fs = require("fs");
 const qs = require("querystring");
 
@@ -26,8 +27,50 @@ const server = http
     //// res.write(file);
     switch (req.method) {
       case "GET":
-        const rs = fs.createReadStream("./form.html");
-        rs.pipe(res);
+        if (req.url === "/") {
+          res.write(
+            '<!DOCTYPE html><html lang="ja"><body>' +
+              "<h1>アンケートフォーム</h1>" +
+              '<a href="/enquetes">アンケート一覧</a>' +
+              "</body></html>"
+          );
+        } else if (req.url === "/enquetes") {
+          res.write(
+            '<!DOCTYPE html><html lang="ja"><body>' +
+              "<h1>アンケート一覧</h1><ul>" +
+              '<li><a href="/enquetes/yaki-shabu">焼き肉・しゃぶしゃぶ</a></li>' +
+              '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+              '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+              "</ul></body></html>"
+          );
+        } else if (req.url === "/enquetes/yaki-shabu") {
+          res.write(
+            pug.renderFile("./form.pug", {
+              path: req.url,
+              firstItem: "焼肉",
+              secondItem: "しゃぶしゃぶ",
+            })
+          );
+        } else if (req.url === "/enquetes/rice-bread") {
+          res.write(
+            pug.renderFile("./form.pug", {
+              path: req.url,
+              firstItem: "ごはん",
+              secondItem: "パン",
+            })
+          );
+        } else if (req.url === "/enquetes/sushi-pizza") {
+          res.write(
+            pug.renderFile("./form.pug", {
+              path: req.url,
+              firstItem: "すし",
+              secondItem: "ぴざ",
+            })
+          );
+        }
+        res.end();
+        //// const rs = fs.createReadStream("./form.html");
+        //// rs.pipe(res);
         break;
       case "POST":
         let rawData = "";
@@ -54,8 +97,8 @@ const server = http
               '<!DOCTYPE html><html lang="ja"><body><h1>' +
                 answer["name"] +
                 "さんは" +
-                answer["yaki-shabu"] +
-                "に投票しました</h1></body></html>"
+                answer["favorite"] +
+                'に投票しました</h1><a href="/enquetes">アンケート一覧に戻る</a></body></html>'
             );
             res.end();
           });
